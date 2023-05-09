5ebc48aa83ca3cdbc2454fc542698462a5386ad7
LeoHach <leonardhach@gmail.com>
2023-05-09 15:06:22 +0200

    Header

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $.ajax({
-        url: 'php/installDB.php',
+        url: '../php/installDB.php',
         type: 'GET',
         success: function (response) {
             console.log(response);
@@ -9,4 +9,44 @@ $(document).ready(function () {
             console.log(xhr.responseText);
         }
     });
+
+    $('.header_icon').hover(
+        function () {
+            $(this).css('transition', 'stroke 0.5s ease').attr('stroke', '#253237');
+        },
+        function () {
+            $(this).css('transition', 'stroke 0.5s ease').attr('stroke', '#ffffff');
+        }
+    );
+
+    $('#header_user').on('click', function () {
+        $("#login_form").slideDown();
+        $('#login_form').css('display', 'flex')
+    });
+
+    $('#login_register').click(function () {
+        //$('#login_form').html($('#register_form').html());
+        $('#login_form').hide();
+        $('#register_form').show();
+        $('#register_form').css('display', 'flex')
+    });
+
+    $("#register_login").click(function () {
+        //$('#register_form').html($('#login_form').html());
+        $('#login_form').show();
+        $('#register_form').hide();
+        $('#login_form').css('display', 'flex')
+
+    });
+
+    $('.dropdown_buttons').hover(
+        function () {
+            $(this).css('transition', 'background-color 0.5s ease').css('background-color', '#253237');
+            $(this).css('transition', 'color 0.5s ease').css('color', '#ffffff');
+        },
+        function () {
+            $(this).css('transition', 'background-color 0.5s ease').css('background-color', '#c2dfe3');
+            $(this).css('transition', 'color 0.5s ease').css('color', '#253237');
+        }
+    );
 });
---
+++ pages/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PaperSolutions</title>
+</head>
+
+<body class="background">
+    <?php include '../components/header.php'; ?>
+    <div class="container">
+        <div class="middle_square">
+
+        </div>
+    </div>
+</body>
+
+</html>
